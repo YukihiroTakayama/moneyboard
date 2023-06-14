32619ab2114287bc05482f916a51d4fe089e073b
Yukihiro Takayama <>
2023-06-14 23:21:40 +0900

    first commit

---
+++ front/src/pages/Maps.vue
@@ -0,0 +1,34 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script>
+  import FullCalendar from '@fullcalendar/vue'
+  import dayGridPlugin from '@fullcalendar/daygrid'
+  import interactionPlugin from '@fullcalendar/interaction'
+  import axios from 'axios'
+
+  export default {
+    components: {
+      FullCalendar
+    },
+    data() {
+      return {
+        calendarOptions: {
+          plugins: [ dayGridPlugin, interactionPlugin ],
+          initialView: 'dayGridMonth',
+          dateClick: this.handleDateClick,
+          events: [
+            { title: 'event 1', date: '2023-06-03', color: 'green' },
+            { title: 'event 2', date: '2023-06-04', color: 'red' }
+          ]
+        }
+      }
+    },
+    methods: {
+      handleDateClick: function(arg) {
+        alert('date click! ' + arg.dateStr)
+      }
+    }
+  }
+</script>
